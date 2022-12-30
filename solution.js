c7fd1240a6b234118c145cdee6a6be31bedc17ef
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2022-12-30 13:11:05 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,16 @@
 // complete the given function
 
 function palindrome(str){
-
+  const palindromeChecker = (str) => {
+  let temp = "";
+  str = str.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    temp += str[i];
+  }
+  for (let i = 0; i < temp.length / 2; i++) {
+    if (temp[i] != temp[temp.length - 1 - i]) return false;
+  }
+  return true;
+  };
 }
 module.exports = palindrome
