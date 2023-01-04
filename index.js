47cda1c6bc27211449bf012f0c41f379edd21898
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-01-04 14:25:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-const isPalindrome = require('./solution');
+const isPalindrome = require('./script');
 
 app.use(express.static(__dirname))
 
